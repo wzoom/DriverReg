e3732d942a3870121f7db417a88b074af1e99518
Pavol Otcenas <palo@festomedia.cz>
2014-05-14 11:12:38 +0200

    First running Angular app

--- js/services.js
+++ js/services.js
@@ -2,9 +2,9 @@
 
 /* Services */
 
-var phonecatServices = angular.module('phonecatServices', ['ngResource']);
+var drServices = angular.module('drServices', ['ngResource']);
 
-phonecatServices.factory('Phone', ['$resource',
+drServices.factory('Phone', ['$resource',
   function($resource){
     return $resource('phones/:phoneId.json', {}, {
       query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
