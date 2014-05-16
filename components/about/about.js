27157cf7d7719099cc2a719b3c277623d3d78e91
Laco Ligart <laco.ligart@adleritech.com>
2014-05-16 08:56:45 +0200

    Summary page

--- components/about/about.js
+++ components/about/about.js
@@ -4,10 +4,10 @@ formAbout.controller('formAboutCtrl', ['$scope', '$rootScope',function($scope, $
   var user = $rootScope.user;
 
     $scope.formItems = [
-        { 'name' : 'Name',
-            'tpl'  : 'components/about/tpl/name.tpl.html'},
         { 'name' : 'Photo',
             'tpl'  : 'components/about/tpl/photo.tpl.html'},
+        { 'name' : 'Name',
+            'tpl'  : 'components/about/tpl/name.tpl.html'},
         { 'name' : 'Phone',
             'tpl'  : 'components/about/tpl/phone.tpl.html'},
         { 'name' : 'Gender',
@@ -15,7 +15,9 @@ formAbout.controller('formAboutCtrl', ['$scope', '$rootScope',function($scope, $
         { 'name' : 'Language',
             'tpl'  : 'components/about/tpl/language.tpl.html'},
         { 'name' : 'Dispatching',
-            'tpl'  : 'components/about/tpl/dispatch.tpl.html'}
+            'tpl'  : 'components/about/tpl/dispatch.tpl.html'},
+        { 'name' : 'Summary',
+            'tpl'  : 'components/about/tpl/summary.tpl.html'}
     ];
 
   // Testing purposes add random string at the end of URL
