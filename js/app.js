b82ab8d53d92a3df5889c3c6952c6965369f9042
Pavol Otcenas <palo@festomedia.cz>
2014-05-23 00:15:17 +0200

    Added states (steps), header menu dynamically loaded, removed controllers, renamed tpl files.

--- js/app.js
+++ js/app.js
@@ -6,29 +6,55 @@ var drApp = angular.module('driverRegApp', [
   'ui.router',
   'mgcrea.ngStrap',
   //'drAnimations',
-  'drControllers',
   'drFilters',
   'drServices',
   'formValidation',
   'formAbout',
+  'start',
   'formVehicle'
 ]);
 
 drApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
   // Default state
-  $urlRouterProvider.otherwise("/about");
+  $urlRouterProvider.otherwise("/start");
 
   $stateProvider
+    .state('start', {
+      title: "Getting Started",
+      url: "/start",
+      templateUrl: 'components/start/start.html?random=' + Math.random(),
+      controller: 'startCtrl'
+    })
     .state('about', {
+      title: "About You",
       url: "/about",
       templateUrl: 'components/about/about.html?random=' + Math.random(),
       controller: 'formAboutCtrl'
     })
     .state('vehicle', {
+      title: "Vehicle",
       url: "/vehicle",
       templateUrl: 'components/vehicle/vehicle.html?random=' + Math.random(),
         controller: 'formVehicleCtrl'
+    })
+    .state('services', {
+      title: "Services",
+      url: "/services",
+      templateUrl: 'components/services/services.html?random=' + Math.random(),
+      controller: 'formServicesCtrl'
+    })
+    .state('prices', {
+      title: "Prices",
+      url: "/prices",
+      templateUrl: 'components/prices/prices.html?random=' + Math.random(),
+      controller: 'formPricesCtrl'
+    })
+    .state('licensing', {
+      title: "Licensing",
+      url: "/licensing",
+      templateUrl: 'components/licensing/licensing.html?random=' + Math.random(),
+      controller: 'formLicensingCtrl'
     })
 
 }]);
@@ -48,17 +74,18 @@ drApp.controller('drAppCtrl', ['$scope', 'User', function($scope, User) {
   });
 }]);
 
-drApp.controller('HeaderCtrl', ['$scope', '$location', '$route',
-  function ($scope, $location, $route) {
-    $scope.location = $location;
-    //$scope.breadcrumbs = breadcrumbs;
+drApp.controller('HeaderCtrl', ['$scope', '$location', '$state', '$filter',
+  function ($scope, $location, $state, $filter) {
+
+    // Fill-in all states (except the virtual root state)
+    $scope.states = $filter('filter')($state.get(),  function(state){return state.name != ''});
+
+
+
+
 
     $scope.isAuthenticated = true;//security.isAuthenticated;
     $scope.isAdmin = true;//security.isAdmin;
-
-    $scope.home = function () {
-      $location.path('/');
-    };
 
     $scope.hideNavbar = function() {
       angular.element('nav.navmenu.offcanvas').offcanvas('hide');
