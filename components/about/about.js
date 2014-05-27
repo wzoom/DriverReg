682f63b1fb74f8e25eb3ed71f09e2c6e9d2d9d70
Laco Ligart <laco.ligart@adleritech.com>
2014-05-27 15:36:16 +0200

    Set up ui-router

--- components/about/about.js
+++ components/about/about.js
@@ -1,108 +1,56 @@
-var formAbout = angular.module('formAbout', [
+angular.module('drApp.about', [
   'ui.router'
-]);
+])
 
-formAbout.controller('formAboutCtrl', ['$scope', '$rootScope',function($scope, $rootScope) {
+.config(['$stateProvider', '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider){
+
+    var stepSettings = {
+      mainState: 'about',
+      firstStep: 'about.photo',
+      stepTitle: 'About you'
+    };
+
+    var getComponentTemplatePath = function (stateName) {
+      return 'components/' + stepSettings.mainState + '/' + stateName + '.html?random=' + Math.random();
+    };
+
+    $stateProvider
+      .state('about', {
+        title: "About You",
+        sideMenu: true,
+        skipAllow: false,
+        url: "/about",
+        templateUrl: getComponentTemplatePath('about'),
+        controller: function ($state) {
+          $state.transitionTo(stepSettings.firstStep);
+        }
+      })
+      .state('about.photo', {
+        title: "Photo",
+        sideMenu: false,
+        skipAllow: true,
+        url: "/photo",
+        templateUrl: getComponentTemplatePath('about.photo')
+      })
+      .state('about.name', {
+        title: "Name",
+        sideMenu: false,
+        skipAllow: false,
+        url: "/name",
+        templateUrl: getComponentTemplatePath('about.name')
+      });
+
+  }
+])
+
+.controller('aboutCtrl', ['$scope', '$rootScope', '$state', '$filter', function ($scope, $rootScope, $state, $filter) {
   var user = $rootScope.user;
 
-  $rootScope.$on('$routeChangeStart', function(next, current) {
-    $scope.step = 0;
-  });
+  $scope.states = $filter('filter')($state.get(),  function(state){return state.name != ''});
+  $scope.currentState = $state.current;
 
-  $rootScope.stepTitle = 'About You';
-
-    $scope.formItems = [
-        { 'name' : 'Photo',
-            'tpl'  : 'components/about/tpl/photo.tpl.html'},
-        { 'name' : 'Name',
-            'tpl'  : 'components/about/tpl/name.tpl.html'},
-        { 'name' : 'Phone',
-            'tpl'  : 'components/about/tpl/phone.tpl.html'},
-        { 'name' : 'Gender',
-            'tpl'  : 'components/about/tpl/gender.tpl.html'},
-        { 'name' : 'Language',
-            'tpl'  : 'components/about/tpl/language.tpl.html'},
-        { 'name' : 'Dispatching',
-            'tpl'  : 'components/about/tpl/dispatch.tpl.html'},
-        { 'name' : 'Summary',
-            'tpl'  : 'components/about/tpl/summary.tpl.html'}
-    ];
-
-  // Testing purposes add random string at the end of URL
-  angular.forEach($scope.formItems, function(value, key){
-    $scope.formItems[key]['tpl'] = value['tpl'] + '?random=' + Math.random();
-  });
-
-    $scope.button = {
-        "visibility": 'yes',
-        "gender" : 'male'
-    };
-
-    $scope.step = 0;
-
-    $scope.isCurrentStep = function(step) {
-        return $scope.step === step;
-    };
-
-    $scope.setCurrentStep = function(step) {
-        $scope.step = step;
-    };
-
-    $scope.getCurrentStep = function() {
-        return $scope.step;
-    };
-
-    $scope.getMaxStep = function() {
-        return $scope.formItems.length - 1;
-    };
-
-    $scope.getCurrentProgress = function() {
-        var perc = $scope.getMaxStep() / 100;
-        return $scope.step / perc + '%';
-    };
-
-    $scope.setNextStep = function() {
-        var nextStep = $scope.step + 1;
-        var maxValue = $scope.getMaxStep();
-        if(nextStep > maxValue) nextStep = maxValue;
-        $scope.step = nextStep;
-    };
+  $rootScope.stepTitle = 'About you';
 
 }]);
 
-formAbout.config(function($stateProvider, $urlRouterProvider) {
-  //
-  // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise("/state1");
-  //
-  // Now set up the states
-  $stateProvider
-    .state('photo', {
-      url: "/photo",
-      templateUrl: "components/about/tpl/photo.tpl.html"
-    })
-    .state('name', {
-      url: "/name",
-      templateUrl: "components/about/tpl/name.tpl.html"
-    })
-    .state('phone', {
-      url: "/phone",
-      templateUrl: "components/about/tpl/phone.tpl.html"
-    })
-    .state('gender', {
-      url: "/gender",
-      templateUrl: "components/about/tpl/gender.tpl.html"
-    })
-    .state('language', {
-      url: "/language",
-      templateUrl: "components/about/tpl/language.tpl.html"
-    })
-    .state('dispatch', {
-      url: "/dispatch",
-      templateUrl: "components/about/tpl/dispatch.tpl.html"
-    })
-    .state('summary', {
-      url: "/summary",
-      templateUrl: "components/about/tpl/summary.tpl.html"
-    })
-});
